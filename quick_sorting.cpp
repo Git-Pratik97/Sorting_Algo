abca357b078b0d3faed00fa5538330ff9ae0763c
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2021-06-24 12:50:48 +0530

    Add files via upload

---
+++ quick_sorting.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+
+    int i = l - 1;
+
+    for(int j =l; j < r; j++)
+    {
+        if(arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i+1,r);
+    return i+1;
+}
+
+void quick_sort(int arr[], int l, int r)
+{
+    if(l < r)
+    {
+        int pi = partition(arr, l, r);
+
+        quick_sort(arr, l, pi-1);
+        quick_sort(arr, pi+1, r);
+
+    }
+}
+
+int main()
+{
+    int arr[7] = {5,4,3,2,1,6,7};
+    quick_sort(arr, 0, 4);
+
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+    
+}
